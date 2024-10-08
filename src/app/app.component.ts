06470a391b074ecdc803e81f8b1f7464f82c40ab
Yusuf Şahin <yunus.ebru@gmail.com>
2024-10-08 21:57:45 +0300

    dashboard sayfasında menü ve footer alanı iptal edildi

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,29 @@
 import { Component, inject } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common'; // CommonModule eklendi
 import { initFlowbite } from 'flowbite';
 import { FlowbiteService } from './services/flowbite.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule], // CommonModule'ü imports'a ekledik
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'admin-joseph';
   flowbiteService = inject(FlowbiteService);
-  
+  router = inject(Router);
+  showMenuAndFooter: boolean = true;
+
+  constructor() {
+    // Router eventlerini dinleyerek, hangi sayfada olduğumuzu kontrol ediyoruz
+    this.router.events.subscribe(() => {
+      this.showMenuAndFooter = this.router.url !== '/dashboard';
+    });
+  }
+
   ngOnInit(): void {
     this.flowbiteService.loadFlowbite(flowbite => {
       console.log('Flowbite yüklendi', flowbite);
