60e36a3452eb2c479b24eea346c7729b3a618ed1
Yusuf Şahin <yunus.ebru@gmail.com>
2024-10-08 21:41:36 +0300

    bir kaç sayfa eklendi

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,12 @@
 import { Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { GaleriComponent } from './galeri/galeri.component';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
 
 export const routes: Routes = [
     {path: "dashboard", component: DashboardComponent},
-    {path: "galeri", component: GaleriComponent}
+    {path: "galeri", component: GaleriComponent},
+    {path: "", component: HomeComponent},
+    {path: "contact", component: ContactComponent}
 ];
